872f83b4ba14cce671c6ef64154a88e99a848792
coding-to-music <connors.tom@gmail.com>
2022-01-19 05:28:38 +0000

    build

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    style: {
-      postcssOptions: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
     },
-  }
+  },
+}
